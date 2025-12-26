b9875f42119f2efdc4b51819f900a6a3b7629f72
Mahir Babbar <mahirbabbar1611@gmail.com>
2025-12-26 12:26:28 +0530

    normalGen

--- Gen.py
+++ Gen.py
@@ -1,11 +1,11 @@
-from caller import receiver
-from scammer import caller
+from receiver import receiver
+from caller import caller
 import os
 import time
 
 os.makedirs("Conv", exist_ok=True)
 
-with open("fraud_context.txt", "r", encoding="utf-8") as f:
+with open("non_fraud_context.txt", "r", encoding="utf-8") as f:
     contexts = f.readlines()
     contexts = contexts[:200]
 
@@ -14,8 +14,8 @@ for j, context in enumerate(contexts, start=1):
     print(f"Simulating conversation {j}...")
 
     for i in range(5, 1, -1):
-        scammer_resp = caller(history, context, i)
-        history += f"caller: {scammer_resp}\n"
+        caller_resp = caller(history, context, i)
+        history += f"caller: {caller_resp}\n"
         time.sleep(6)
         receiver_resp = receiver(history)
         history += f"receiver: {receiver_resp}\n"
